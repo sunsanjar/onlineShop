31e4f9e987f9e57cf9b1a6e38c44b29ace0b6fe2
sunsanjar <sunsanjar@mail.ru>
2021-02-05 20:34:35 +0600

    ssss

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(".lazy").slick({
+		lazyLoad: 'ondemand', // ondemand progressive anticipated
+		infinite: true,
+		dots: false,
+		autoplay: true,
+        arrows: false,
+        speed: 300,
+		slidesToShow: 1,
+		autoplaySpeed: 5000,
+        adaptiveHeight: true
+	});
